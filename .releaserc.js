1fdc1855746e0c1c082a339cc30d2b8b8cfa414a
Fernando Rojo <frojo@sas.upenn.edu>
2021-07-08 15:02:51 -0600

    native: full increment on prerelease test? should be 4.0.0-eas.1

--- .releaserc.js
+++ .releaserc.js
@@ -7,7 +7,7 @@ const rules = [
   { type: "docs", release: "patch", title: "Documentation changes" },
   { type: "native", release: "major" },
 ];
-// master
+// major
 
 // Simple mapping to order the commit groups
 const sortMap = Object.fromEntries(
