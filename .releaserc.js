df44047ded2198461f634521336be4599d515084
Fernando Rojo <frojo@sas.upenn.edu>
2021-07-08 14:28:57 -0600

    native: test major pre-release?

--- .releaserc.js
+++ .releaserc.js
@@ -7,7 +7,7 @@ const rules = [
   { type: "docs", release: "patch", title: "Documentation changes" },
   { type: "native", release: "major" },
 ];
-// master
+// master-prerelease
 
 // Simple mapping to order the commit groups
 const sortMap = Object.fromEntries(
